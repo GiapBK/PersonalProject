8119eacd4fc1561b11244e05dba9baaf98f56e77
GiapBK <fruity.giapnd@gmail.com>
2015-09-15 11:29:47 +0700

    Set up Project

---
+++ PersonalProject/PersonalProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonalProject
+//
+//  Created by Giap Nguyen on 9/15/15.
+//  Copyright (c) 2015 co.iktv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonalProject/PersonalProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonalProject
+//
+//  Created by Giap Nguyen on 9/15/15.
+//  Copyright (c) 2015 co.iktv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
